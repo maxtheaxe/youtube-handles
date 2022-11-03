8cbc1d0b59c7acd7b84da34a21dc2adff87ebbf2
Max Perrello <max.perrello@gmail.com>
2022-11-04 01:38:21 +0600

    add logout handling

--- handle_search.py
+++ handle_search.py
@@ -1,3 +1,4 @@
+import csv
 from glob import glob
 from haralyzer import HarParser
 from json import loads
@@ -6,7 +7,7 @@ import pickle
 import requests
 
 
-def get_latest_har():
+def get_latest_har(log):
 	all_files = glob(os.getcwd() + "/*")  # get list of files in own folder
 	har_files = []
 	for file in all_files:
@@ -14,10 +15,24 @@ def get_latest_har():
 		if file.lower().endswith('.har') and ("youtube.com" in file.lower()):
 			har_files.append(file)
 	latest_har = max(har_files, key=os.path.getctime)
+	if log:
+		print(f"using latest HAR file: {latest_har}")
 	return latest_har
 
 
-def convert_headers(headers, want_cookies = True):
+def import_usernames(filename, log):
+	usernames = []
+	num_names = 0
+	with open(filename) as csv_file:
+		for row in csv_file:
+			if len(row[:-1]) >= 3:  # don't import usernames less than three chars
+				usernames.append(row[:-1])
+	if log:
+		print(f"successfully imported {num_names} usernames from {filename}")
+	return usernames
+
+
+def convert_headers(headers, want_cookies):
 	# originally compressed, which postman and ff handled automatically
 	# caused quite the headache figuring out why it couldn't be decoded
 	unwanted_headers = ["Content-Length", "Accept-Encoding"]
@@ -30,55 +45,85 @@ def convert_headers(headers, want_cookies = True):
 	return new_headers
 
 
-def import_request(filename):
+def import_request(filename, want_cookies):
 	# verify that it was a successful one(?)
 	har_parser = HarParser.from_file(filename)
 	# there should only be one request collected (for one page)
 	target_request = har_parser.pages[0].post_requests[0]
-	return target_request.url, convert_headers(target_request.request.headers), \
-		loads(target_request.request["postData"]["text"])
+	return target_request.url, \
+		   convert_headers(target_request.request.headers, want_cookies), \
+		   loads(target_request.request["postData"]["text"])
 
 
-def save_session(session):
-	with open('cookies.pickle', 'wb') as f:
-		pickle.dump(session.cookies, f)
-
-
-def load_session():
+def load_session(log=False):
 	session = requests.session()  # or an existing session
 	try:  # check if we've already saved updated cookies
 		with open('cookies.pickle', 'rb') as f:
 			session.cookies.update(pickle.load(f))
 	except FileNotFoundError:  # otherwise, use those from HAR
 		return session, False
+	if log:
+		print("using existing cookies from previous usage")
 	return session, True
 
 
-def perform_request(session, url, headers, payload, username):
+def save_session(session, log):
+	with open('cookies.pickle', 'wb') as f:
+		pickle.dump(session.cookies, f)
+	if log:
+		print("cookies successfully saved")
+
+
+def delete_session(log):
+	os.remove("cookies.pickle")
+	if log:
+		print("cookies successfully removed")
+
+
+def perform_request(session, url, headers, payload, username, log):
 	payload["handle"] = username  # replace previous username with desired search term
 	response = session.post(url, headers=headers, json=payload)
 	response_data = loads(response.content.decode("latin-1"))
-	save_session(session)
+	# print("response data:\n", response_data)
+	save_session(session, log)
 	# print(f"session cookies:\n{session.cookies}")
-	if response_data["result"]["channelHandleValidationResultRenderer"]["result"]\
-		== "CHANNEL_HANDLE_VALIDATION_RESULT_OK":
-		return True, session
+	try:
+		if response_data["result"]["channelHandleValidationResultRenderer"]["result"] \
+				== "CHANNEL_HANDLE_VALIDATION_RESULT_OK":
+			return True, session
+	except KeyError as err:
+		print(err)
+		if (err.args[0] == "result") and (response.status_code == 401):
+			delete_session(log) # old cookies are stale, we'll get new ones from HAR
+			raise Exception("time to download a new HAR file!" + \
+							" (youtube logged you out, be careful)")
+		else:
+			raise Exception("unknown error occurred—youtube must've changed their API" + \
+							" (please tell me!)")
 	return False, session
 
 
-def main():
-	latest_har = get_latest_har()
+def run_full_search(usernames, log):
+	latest_har = get_latest_har(log)
 	session, session_existed = load_session()
-	url, headers, payload = import_request(latest_har)
-	new_usernames = ["mnawdhabwdaw7183", "hello", "silly", "stinky"]
-	for username in new_usernames:
-		success, session = perform_request(session, url, headers, payload, username)
-		success_string = ""
-		if not success:
-			success_string = "not "
-		# only print "not" if it fails
-		# (tried to use ternary operator in fstring, but it looked gross)
-		print(f"the handle '{username}' is {success_string}available")
+	# if session already existed, we don't collect cookies from HAR
+	url, headers, payload = import_request(latest_har, (not session_existed))
+	for username in usernames:
+		success, session = perform_request(session, url, headers, payload, username, log)
+		if log:
+			success_string = ""
+			if not success:
+				success_string = "not "
+			# only print "not" if it fails
+			# (tried to use ternary operator in fstring, but it looked gross)
+			print(f"the handle '{username}' is {success_string}available")
+
+
+def main():
+	log = True  # set to true if you want to print program logs
+	usernames = ["smelly", "tiola1396u", "lolman"]
+	# usernames = import_usernames("usernames.csv")
+	run_full_search(usernames, log)
 
 
 if __name__ == "__main__":
