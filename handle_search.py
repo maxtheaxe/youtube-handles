1a9d3aba603b751d8d8a9773c08c66cab38ffbfb
Max Perrello <max.perrello@gmail.com>
2022-11-03 23:54:42 +0600

    automatically get latest har file, persist cookies

--- handle_search.py
+++ handle_search.py
@@ -1,17 +1,32 @@
+from glob import glob
 from haralyzer import HarParser
-from json import loads, dumps
+from json import loads
+import os
 import pickle
 import requests
 
 
-def convert_headers(headers):
+def get_latest_har():
+	all_files = glob(os.getcwd() + "/*")  # get list of files in own folder
+	har_files = []
+	for file in all_files:
+		# if file is a youtube.com har file, save it
+		if file.lower().endswith('.har') and ("youtube.com" in file.lower()):
+			har_files.append(file)
+	latest_har = max(har_files, key=os.path.getctime)
+	return latest_har
+
+
+def convert_headers(headers, want_cookies = True):
+	# originally compressed, which postman and ff handled automatically
+	# caused quite the headache figuring out why it couldn't be decoded
 	unwanted_headers = ["Content-Length", "Accept-Encoding"]
+	if not want_cookies:
+		unwanted_headers.append("Cookie")
 	new_headers = {}
 	for kvp in headers:
 		if kvp["name"] not in unwanted_headers:
 			new_headers[kvp["name"]] = kvp["value"]
-		# else:
-		# 	new_headers["Content-Length"]
 	return new_headers
 
 
@@ -20,11 +35,13 @@ def import_request(filename):
 	har_parser = HarParser.from_file(filename)
 	# there should only be one request collected (for one page)
 	target_request = har_parser.pages[0].post_requests[0]
-	# print("url:\n", target_request.request.url)
-	# print("headers:\n", convert_headers(target_request.request.headers))
-	# print("payload:\n", loads(target_request.request["postData"]["text"]))
 	return target_request.url, convert_headers(target_request.request.headers), \
 		loads(target_request.request["postData"]["text"])
+
+
+def save_session(session):
+	with open('cookies.pickle', 'wb') as f:
+		pickle.dump(session.cookies, f)
 
 
 def load_session():
@@ -37,55 +54,31 @@ def load_session():
 	return session, True
 
 
-def conduct_request(session, url, headers, payload, username):
+def perform_request(session, url, headers, payload, username):
 	payload["handle"] = username  # replace previous username with desired search term
-	response = requests.post(url, headers=headers, json=payload)
-	# print("request url:\n", response.request.url)
-	# print("request headers:\n", response.request.headers)
-	# print("request data:\n", response.request.body)
-	# print("response headers:\n", response.headers)
-	# print("response status:\n", response.status_code)
-	# print("response encoding:\n", response.encoding)
-	# print("raw response:\n", response.content)
-	# from charset_normalizer import detect, normalize
-	# print(f"actual char type: {detect(response.content)}")
+	response = session.post(url, headers=headers, json=payload)
 	response_data = loads(response.content.decode("latin-1"))
+	save_session(session)
+	# print(f"session cookies:\n{session.cookies}")
 	if response_data["result"]["channelHandleValidationResultRenderer"]["result"]\
 		== "CHANNEL_HANDLE_VALIDATION_RESULT_OK":
-		return True
-	return False
-
-
-
-def pretty_print_POST(req):
-	"""
-	At this point it is completely built and ready
-	to be fired; it is "prepared".
-
-	However pay attention at the formatting used in
-	this function because it is programmed to be pretty
-	printed and may differ from the actual request.
-	"""
-	# pretty_print_POST(requests.Request('POST', url, headers=headers, data = payload).prepare())
-	print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-		'-----------START-----------',
-		req.method + ' ' + req.url,
-		'\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-		req.body,
-	))
+		return True, session
+	return False, session
 
 
 def main():
+	latest_har = get_latest_har()
 	session, session_existed = load_session()
-	new_username = "mnawdhabwdaw7183"
-	url, headers, payload = import_request("www.youtube.com_Archive [22-11-03 21-53-57].har")
-	success = conduct_request(session, url, headers, payload, new_username)
-	success_string = ""
-	if not success:
-		success_string = "not "
-	# only print "not" if it fails
-	# (tried to use ternary operator in fstring, but it looked gross)
-	print(f"the handle '{new_username}' is {success_string}available")
+	url, headers, payload = import_request(latest_har)
+	new_usernames = ["mnawdhabwdaw7183", "hello", "silly", "stinky"]
+	for username in new_usernames:
+		success, session = perform_request(session, url, headers, payload, username)
+		success_string = ""
+		if not success:
+			success_string = "not "
+		# only print "not" if it fails
+		# (tried to use ternary operator in fstring, but it looked gross)
+		print(f"the handle '{username}' is {success_string}available")
 
 
 if __name__ == "__main__":
