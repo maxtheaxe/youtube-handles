b2e5b02c33c15d092725e9ed0131c1368be320f6
Max Perrello <max.perrello@gmail.com>
2022-11-04 08:05:53 +0600

    pickle entire sessions

--- handle_search.py
+++ handle_search.py
@@ -44,11 +44,13 @@ def save_results(results, log):
 		print(f"successfully saved {len(results)} results")
 
 
-def convert_headers(headers, want_cookies):
+def convert_headers(headers, want_cookies, log):
 	# originally compressed, which postman and ff handled automatically
 	# caused quite the headache figuring out why it couldn't be decoded
 	unwanted_headers = ["Content-Length", "Accept-Encoding"]
 	if not want_cookies:
+		if log:
+			print("not using cookies from HAR file")
 		unwanted_headers.append("Cookie")
 	new_headers = {}
 	for kvp in headers:
@@ -57,39 +59,41 @@ def convert_headers(headers, want_cookies):
 	return new_headers
 
 
-def import_request(filename, want_cookies):
+def import_request(filename, want_cookies, log):
 	# verify that it was a successful one(?)
 	har_parser = HarParser.from_file(filename)
 	# there should only be one request collected (for one page)
 	target_request = har_parser.pages[0].post_requests[0]
 	return target_request.url, \
-		   convert_headers(target_request.request.headers, want_cookies), \
+		   convert_headers(target_request.request.headers, want_cookies, log), \
 		   loads(target_request.request["postData"]["text"])
 
 
-def load_session(log=False):
-	session = requests.session()  # or an existing session
+def load_session(log):
 	try:  # check if we've already saved updated cookies
-		with open("cookies.pickle", "rb") as f:
-			session.cookies.update(pickle.load(f))
+		with open("session.pickle", "rb") as f:
+			session = pickle.load(f)
 	except FileNotFoundError:  # otherwise, use those from HAR
+		session = requests.session()  # or an existing session
+		if log:
+			print("no session found")
 		return session, False
 	if log:
-		print("using existing cookies from previous usage")
+		print("using existing session from previous usage")
 	return session, True
 
 
 def save_session(session, log):
-	with open("cookies.pickle", "wb") as f:
-		pickle.dump(session.cookies, f)
+	with open("session.pickle", "wb") as f:
+		pickle.dump(session, f)
 	if log:
-		print("cookies successfully saved")
+		print("session successfully saved")
 
 
 def delete_session(log):
-	os.remove("cookies.pickle")
+	os.remove("session.pickle")
 	if log:
-		print("cookies successfully removed")
+		print("session successfully removed")
 
 
 def check_username(session, url, headers, payload, username, log):
@@ -108,7 +112,7 @@ def check_username(session, url, headers, payload, username, log):
 		if response.status_code == 401:
 			print("logged out—saving and shutting down")
 		elif response.status_code == 429:
-			print("rate limit! sleeping...")
+			print("rate limit! sleeping for", end="")
 		else:
 			print(f"unknown error #{status}, save this message: {response.content}")
 	return False, session, status
@@ -116,9 +120,9 @@ def check_username(session, url, headers, payload, username, log):
 
 def run_full_search(usernames, log):
 	latest_har = get_latest_har(log)
-	session, session_existed = load_session()
+	session, session_existed = load_session(log)
 	# if session already existed, we don't collect cookies from HAR
-	url, headers, payload = import_request(latest_har, (not session_existed))
+	url, headers, payload = import_request(latest_har, (not session_existed), log)
 	results = []
 	rate_limit_counter = 0
 	for username in usernames:
@@ -138,10 +142,12 @@ def run_full_search(usernames, log):
 		elif status == 429:  # rate limit, slowing down
 			rate_limit_counter += 1
 			# if already limit 10 times (even with delays), exit
-			if rate_limit_counter >= 10:
+			if rate_limit_counter >= 3:
 				return results, status
 			# sleep for progressively more time (with random element)
-			time.sleep((rate_limit_counter + 1) * (60 + random_element))
+			total_time = ((rate_limit_counter + 1) * (60 + random_element))
+			print(f" {total_time} seconds...")
+			time.sleep(total_time)
 		else:  # something went wrong, exit gracefully
 			return results, status
 	return results, 200
@@ -155,11 +161,11 @@ def main():
 	save_results(results, log)  # log what we have, regardless of whether completed
 	if status != 200:  # if something went wrong, search exited early
 		if status == 401:  # logged out condition
-			delete_session(log)  # old cookies are stale, we'll get new ones from HAR
+			delete_session(log)  # old session is stale, we'll get new one from HAR
 			raise Exception("time to download a new HAR file!" + \
 							" (youtube logged you out, be careful)")
 		elif status == 429:  # rate limit condition
-			raise Exception("you've been rate limited 10 times already!" + \
+			raise Exception("you've been rate limited three times already!" + \
 							" if I were you, I'd take the rest of the day off")
 		else:  # unknown error condition
 			raise Exception("unknown error occurred—youtube must've changed their API" + \
